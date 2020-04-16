bf35ad10b429b35a84c44e86ffb1c9d50a4d4469
youngme92 <youngme992@gmail.com>
2020-04-16 19:00:27 +0900

    Updates

---
+++ precache-manifest.35b26149f4a2e5d58f7e82951aff35f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733bfed15dac67401bc55bf9a9ed789c",
+    "url": "/movie_application/index.html"
+  },
+  {
+    "revision": "e310ad8cebf563f9ab22",
+    "url": "/movie_application/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e594e47fc672372e0ebd",
+    "url": "/movie_application/static/css/main.45c6200c.chunk.css"
+  },
+  {
+    "revision": "e310ad8cebf563f9ab22",
+    "url": "/movie_application/static/js/2.db242605.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_application/static/js/2.db242605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e594e47fc672372e0ebd",
+    "url": "/movie_application/static/js/main.3d692e2b.chunk.js"
+  },
+  {
+    "revision": "34f941d0501107e0c666",
+    "url": "/movie_application/static/js/runtime-main.3d771276.js"
+  }
+]);
