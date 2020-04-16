c8ecc2d2f4ce49a19fa2341e533cd58ae0cb6064
youngme92 <youngme992@gmail.com>
2020-04-16 19:01:15 +0900

    building the Router

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import './routes/About';
+import './routes/Home';
+import Home from './routes/Home';
+import About from './routes/About';
 
-
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  }
-// movies를 movies 한개만 둬도 가능
-  getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json")
-    this.setState({movies : movies, isLoading: false})
-  }
-
-  componentDidMount(){
-    console.log("movies update!")
-    this.getMovies();
-  }
-  componentDidUpdate(){
-    console.log(this.state.movies)
-  }
-render() {
-    const {isLoading, movies} = this.state;
-    return <section className="container">{
-            isLoading
-                ? <div className="loader">
-                        <span className="loader__text">Loading...</span>
-                    </div>
-                : (
-                    <div className="movies">
-                        {
-                            movies.map(movies => {
-                                console.log(movies)
-                                return <Movie
-                                    key={movies.id}
-                                    id={movies.id}
-                                    url={movies.url}
-                                    title={movies.title}
-                                    year={movies.year}
-                                    summary={movies.summary}
-                                    poster={movies.medium_cover_image}
-                                    genres={movies.genres}/>
-                            })
-                        }</div>
-                )
-        }
-    </section>
-}
-}
+function App(){
+  return (
+  <HashRouter>
+    <Route path='/' exact={true} component={Home} />
+    <Route path='/about' exact={true} component={About} />  
+  </HashRouter>
+  )}
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
+
+
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+// movies를 movies 한개만 둬도 가능
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json")
+    this.setState({movies : movies, isLoading: false})
+  }
+
+  componentDidMount(){
+    console.log("movies update!")
+    this.getMovies();
+  }
+  componentDidUpdate(){
+    console.log(this.state.movies)
+  }
+render() {
+    const {isLoading, movies} = this.state;
+    return <section className="container">{
+            isLoading
+                ? <div className="loader">
+                        <span className="loader__text">Loading...</span>
+                    </div>
+                : (
+                    <div className="movies">
+                        {
+                            movies.map(movies => {
+                                console.log(movies)
+                                return <Movie
+                                    key={movies.id}
+                                    id={movies.id}
+                                    url={movies.url}
+                                    title={movies.title}
+                                    year={movies.year}
+                                    summary={movies.summary}
+                                    poster={movies.medium_cover_image}
+                                    genres={movies.genres}/>
+                            })
+                        }</div>
+                )
+        }
+    </section>
+}
+}
+export default Home;
