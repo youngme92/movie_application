742f6fe2f84d09fe69d59a7061f73fe33538f99c
youngme92 <youngme992@gmail.com>
2020-04-17 16:04:29 +0900

    Navigation setting

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {HashRouter, Route} from 'react-router-dom';
 import './routes/About';
 import './routes/Home';
+import Navigation from './components/Navigation'
+import './App.css'
 import Home from './routes/Home';
 import About from './routes/About';
+
+
 
 function App(){
   return (
   <HashRouter>
+    <Navigation />
     <Route path='/' exact={true} component={Home} />
     <Route path='/about' exact={true} component={About} />  
   </HashRouter>
